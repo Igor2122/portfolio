7195e39ebec23385ce22ac09ff5d4007877b27fe
Iogr Pavlenko <igor_pavlenko@yahoo.com>
2018-07-17 15:54:37 +0000

    added work section, basic animations, restructured flexbox

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+
+// const menuOverlay = document.querySelector('.text-center');
+// const animatedDiv = document.querySelector('.menu-overlay');
+
+// console.log(menuOverlay);
+
+// menuOverlay.addEventListener('click', function(){
+//     alert('hi ther');
+// });
+
+// function animateDiv () {
+//     alert('hi there');
+// }
+
