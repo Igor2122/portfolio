e6ca7179231a2eedb79fe55730320e02d3599c2d
Iogr Pavlenko <igor_pavlenko@yahoo.com>
2018-07-20 06:10:14 +0000

    animation updates on experience section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
-
 // Work Section div animations 
 
 const jsIcon = document.querySelector('.js');
 const hmtlIcon = document.querySelector('.html');
+const itemDiv = document.querySelector('.item');
 
-    // Event lisntner 
-    
-document.querySelector('.itme').addEventListener('mouseover', animate);
+// Event lisntner 
+
+document.querySelector('.item').addEventListener('mouseenter', animate_item);
+
+document.querySelector('.item').addEventListener('mouseout', unAnimate);
+// document.getElementById('sections').addEventListener('mouseover', unAnimate);
+
+console.log(itemDiv.style.width);
+
+function translateElemtns() {
+        jsIcon.setAttribute('style', 'transform: translateX(-100%) rotate(-45deg);');
+        hmtlIcon.setAttribute('style', 'transform: translateX(100%) rotate(45deg);');
+        itemDiv.style.transform = "rotate(7deg)";
+        itemDiv.style.transform = "scale(1.2)";
+
+
+function animate_item() {
+    setTimeout(translateElemtns
+    , 500);
+
+};
+
+function unAnimate() {
+    jsIcon.setAttribute('style', 'transform: translateX(0%) rotate(0deg);');
+    hmtlIcon.setAttribute('style', 'transform: translateX(0%) rotate(0deg);');
+    console.log('leave');
+    itemDiv.style.transform = "rotate(0deg)";
+}
