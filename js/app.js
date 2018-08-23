977da2f8fb4e86e6648069114794577c986cf3d2
Igor <igor_pavlenko@yahoo.com>
2018-08-23 13:29:39 +0300

    updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Animations to the links on Services page 
 const items = document.querySelectorAll('.item');
-
+const buttonsDiv = document.querySelector('.buttons-div');
+const buttons = document.querySelectorAll('.about-button');
 
 
 class Ui {
@@ -12,22 +13,29 @@ class Ui {
         target.style.left = '';
         target2.style.top = '';
     }
+
+    btnOver(e) {
+        e.target.style.background = 'red';
+    }
+    btnLeave(e) {
+        e.target.style.background = '';
+    }
 }
 
 // get event listeners 
 for (let i = 0; i < items.length; i++) {
     const ui = new Ui();
     items[i].addEventListener('mouseenter', function (e) {
-        
+
         const link = e.target.childNodes[7];
         const link2 = e.target.childNodes[1];
         console.log(link2);
-        
+
         if (e.target.classList.contains('item')) {
             ui.animate(link, link2);
             console.log('entered');
         }
-        
+
     });
     items[i].addEventListener('mouseleave', function (e) {
         const link2 = e.target.childNodes[1];
@@ -37,66 +45,13 @@ for (let i = 0; i < items.length; i++) {
     })
 }
 
-
-// document.getElementById('secitons').addEventListener('mouseleave', animFinish);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Work Section div animations 
-
-// const jsIcon = document.querySelector('.js');
-// const hmtlIcon = document.querySelector('.html');
-// const itemDiv = document.querySelectorAll('.item');
-
-// // Event lisntner 
-// let allItems; 
-
-// for(let i = 0; i < itemDiv.length; i++){
-//     itemDiv[i].addEventListener('mouseout', unAnimate);
-//     itemDiv[i].addEventListener('mouseenter', animate_item);
-// }
-
-// let anim1;
-// let anim2;
-// let anim3;
-
-// const animationForward = function translateElemtns() {
-//     anim1 = setTimeout(function(){
-//         jsIcon.setAttribute('style', 'transform: translateX(-100%) rotate(-45deg) ;');
-//     }, 50);
-//     hmtlIcon.setAttribute('style', 'transform: translateX(100%) rotate(45deg);');
-//     // itemDiv.style.transform = "rotate(7deg)";
-// }
-
-// const animationBackward = function () {
-//     jsIcon.setAttribute('style', 'transform: translateX(0%) rotate(0deg);');
-//     hmtlIcon.setAttribute('style', 'transform: translateX(0%) rotate(0deg);');
-
-// }
-
-// let anim;
-
-// function animate_item() {
-//     // itemDiv.style.transform = "scale(1.2)";
-//     anim = setTimeout('animationForward()', 200);
-// };
-
-// function unAnimate() {
-//     clearTimeout(anim);
-//     animationBackward();
-// }
+// buttons events 
+for (let i = 0; i < buttons.length; i++) {
+    const ui = new Ui();
+    buttons[i].addEventListener('mouseenter', function () {
+        ui.btnOver(event);
+    });
+    buttons[i].addEventListener('mouseleave', function () {
+        ui.btnLeave(event);
+    });
+}
